88ebecd5d7fd35d996fb1792ca13cef8b59165d0
Rasmi U <rasmi13ncb@gmail.com>
2023-02-13 12:18:37 +0530

    Add project files.

---
+++ ScrapingWikipedia/Program.cs
@@ -0,0 +1,41 @@
+﻿using HtmlAgilityPack;
+using CsvHelper;
+using ScrapySharp.Extensions;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Formats.Asn1;
+using System.Text;
+
+namespace ScrapingWikipedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Count = 0;
+            var HtmlURL = "https://en.wikipedia.org/wiki/Computer_science";
+            HtmlWeb Web = new HtmlWeb();
+            var HtmlDoc = Web.Load(HtmlURL);
+
+            var Links = HtmlDoc.DocumentNode.SelectNodes("//div[@id='mw-content-text']//div[@class='mw-parser-output']//p[2]//a");
+            
+            var RefLinks = new List<string>();
+            foreach(var Link in Links)
+            {
+                Count++;
+                RefLinks.Add(Link.Attributes["href"].Value);
+                Console.WriteLine(Link.Attributes["href"].Value);
+            }
+            Console.WriteLine("Count : {0}", Count);
+
+            StringBuilder sb = new StringBuilder();
+            foreach(var RefLink in RefLinks)
+            {
+                sb.AppendLine(RefLink);
+            }
+
+            File.WriteAllText("ReferenceLinks.txt", sb.ToString());
+        }
+    }
+}
